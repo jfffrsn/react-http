2f0a7539eb35fc73677dcdfe66b30cda615492da
jeffferson99 <49041684+jeffferson99@users.noreply.github.com>
2019-12-17 01:27:05 -0500

    Adding Interceptors to Execute Code Globally

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -25,10 +25,11 @@ class FullPost extends Component {
   }
 
   deletePostHandler = () => {
-    axios.delete("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
-    .then(response => {
+    axios
+      .delete("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
+      .then(response => {
         console.log(response);
-    });
+      });
   };
 
   render() {
